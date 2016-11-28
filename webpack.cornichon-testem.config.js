1d56c7d024f8b17c7ba0dac973010fcdf71deae9
Robbie Pitts <me@robbie-pitts.com>
2016-11-28 09:19:51 -0500

    Simple testem integration

---
+++ webpack.cornichon-testem.config.js
@@ -0,0 +1,46 @@
+var webpack = require("webpack");
+var path = require('path');
+
+module.exports = {
+  entry: {
+    features: './tests/features/index.js',
+    adapter: './tests/features/testem-adapter.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'tmp'),
+    filename: '[name].js',
+  },
+  target: 'web',
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: require.resolve('./src/cornichon'),
+        loader: 'expose-loader?cornichon',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    root: [
+      path.resolve(__dirname),
+      path.resolve(__dirname, 'src'),
+    ],
+    modulesDirectories: ['node_modules',],
+  },
+  eslint: {
+    configFile: '.eslintrc.js',
+  },
+};
